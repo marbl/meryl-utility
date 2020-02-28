9c35952e5dc1bc6a74bfc4879dcffca10c9c4e2c
Brian P. Walenz <thebri@gmail.com>
2020-02-28 11:05:14 -0500

    stuffedBits: Optimizations for when 0 bits are supplied.

--- src/utility/bits.C
+++ src/utility/bits.C
@@ -638,6 +638,9 @@ uint64
 stuffedBits::getBinary(uint32 width) {
   uint64  value = 0;
 
+  if (width == 0)
+    return(0);
+
   assert(width < 65);
 
   //  Ensure we're in valid data.
@@ -702,6 +705,9 @@ stuffedBits::getBinary(uint32 width, uint64 number, uint64 *values) {
 uint32
 stuffedBits::setBinary(uint32 width, uint64 value) {
 
+  if (width == 0)
+    return(0);
+
   assert(width < 65);
 
   ensureSpace(width);
@@ -755,6 +761,9 @@ stuffedBits::setBinary(uint32 width, uint64 value) {
 uint32
 stuffedBits::setBinary(uint32 width, uint64 number, uint64 *values) {
   uint32 size = 0;
+
+  if (width == 0)
+    return(0);
 
   for (uint64 ii=0; ii<number; ii++)
     size += setBinary(width, values[ii]);

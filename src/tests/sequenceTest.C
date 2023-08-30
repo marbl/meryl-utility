ed0ec8d75236ce050d424dcde5d6dfb40c032a52
Brian P. Walenz <thebri@gmail.com>
2023-08-30 01:38:13 -0400

    Fix encode2bitBase() to be valid C++.

--- src/tests/sequenceTest.C
+++ src/tests/sequenceTest.C
@@ -21,8 +21,8 @@
 
 using namespace merylutil::sequence;
 
-int
-main(int argc, char **argv) {
+bool
+testLoadSeq(void) {
   FILE *O;
 
   O = merylutil::openOutputFile("sequenceTest.data.fasta");
@@ -63,6 +63,90 @@ main(int argc, char **argv) {
 
   fprintf(stderr, "Success!\n");
 
-  return(0);
+  return true;
 }
 
+
+
+bool
+encodeTest(void) {
+
+  fprintf(stderr, "%c=%02x %c=%02x %c=%02x %c=%02x %c=%02x\n",
+          decode2bitBase(0x00), encode2bitBase(decode2bitBase(0x00)),
+          decode2bitBase(0x01), encode2bitBase(decode2bitBase(0x01)),
+          decode2bitBase(0x02), encode2bitBase(decode2bitBase(0x02)),
+          decode2bitBase(0x03), encode2bitBase(decode2bitBase(0x03)),
+          decode2bitBase(0x04), encode2bitBase(decode2bitBase(0x04)));
+
+  assert(encode2bitBase(decode2bitBase(0x00)) == 0x00);
+  assert(encode2bitBase(decode2bitBase(0x01)) == 0x01);
+  assert(encode2bitBase(decode2bitBase(0x02)) == 0x02);
+  assert(encode2bitBase(decode2bitBase(0x03)) == 0x03);
+  assert(encode2bitBase(decode2bitBase(0x04)) == 0x04);
+
+  assert(encode2bitBase('a') == 0x00);
+  assert(encode2bitBase('c') == 0x01);
+  assert(encode2bitBase('t') == 0x02);
+  assert(encode2bitBase('g') == 0x03);
+  assert(encode2bitBase('n') == 0x04);
+
+  assert(encode2bitBase('A') == 0x00);
+  assert(encode2bitBase('C') == 0x01);
+  assert(encode2bitBase('T') == 0x02);
+  assert(encode2bitBase('G') == 0x03);
+  assert(encode2bitBase('N') == 0x04);
+
+  assert(decode2bitBase(0x00) == 'A');
+  assert(decode2bitBase(0x01) == 'C');
+  assert(decode2bitBase(0x02) == 'T');
+  assert(decode2bitBase(0x03) == 'G');
+  assert(decode2bitBase(0x04) == 'N');
+
+
+  fprintf(stdout, "\n");
+  fprintf(stdout, "     ASCII                    encode2bitBase()         decode2bitBase()\n");
+  fprintf(stdout, "   \\_0123456789abcdef       \\_0123456789abcdef       \\_0123456789abcdef\n");
+  for (uint32 ii=0; ii<16; ii++) {
+    fprintf(stdout, "0x%01x| ", ii);
+    for (uint32 jj=0; jj<16; jj++)
+      fprintf(stdout, "%c", isVisible(ii*16 + jj) ? (ii*16 + jj) : '.');
+
+    fprintf(stdout, "    0x%01x| ", ii);
+    for (uint32 jj=0; jj<16; jj++)
+      fprintf(stdout, "%c", encode2bitBase(ii*16 + jj) + '0');
+
+    fprintf(stdout, "    0x%01x| ", ii);
+    for (uint32 jj=0; jj<16; jj++)
+      fprintf(stdout, "%c", decode2bitBase(ii*16 + jj));
+    fprintf(stdout, "\n");
+  }
+
+
+  return true;
+}
+
+
+
+int
+main(int argc, char **argv) {
+
+  int arg=1;
+  while (arg < argc) {
+    if      (strcmp(argv[arg], "-l") == 0) {
+      testLoadSeq();
+    }
+    else if (strcmp(argv[arg], "-e") == 0) {
+      encodeTest();
+    }
+    else {
+      fprintf(stderr, "usage: %s -l | -t\n", argv[0]);
+      return 1;
+    }
+
+    arg++;
+  }
+
+
+
+  return 0;
+}

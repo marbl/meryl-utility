21b4da4c04e6cd5652a2f8059c04feb4c7621936
Brian P. Walenz <thebri@gmail.com>
2023-09-07 17:22:01 -0400

    Fix dumps to support v4 databases.

--- src/kmers-v2/kmers-reader-dump.C
+++ src/kmers-v2/kmers-reader-dump.C
@@ -64,8 +64,8 @@ dumpMerylDataFile(char *name) {
   while (D->loadFromFile(F)) {
     uint64 position   = D->getPosition();
 
-    uint64 m1         = D->getBinary(64);
-    uint64 m2         = D->getBinary(64);
+    uint64 m1         = D->getBinary(64), M1 = 0x7461446c7972656dllu;
+    uint64 m2         = D->getBinary(64), M2 = 0x0a3130656c694661llu;
 
     uint64 prefix     = D->getBinary(64);
     uint64 nKmers     = D->getBinary(64);
@@ -79,11 +79,13 @@ dumpMerylDataFile(char *name) {
     uint64 c1         = D->getBinary(64);
     uint64 c2         = D->getBinary(64);
 
-    if ((m1 != 0x7461446c7972656dllu) ||
-        (m2 != 0x0a3030656c694661llu)) {
-      fprintf(stderr, "merylFileReader::nextMer()-- Magic number mismatch at position " F_U64 ".\n", position);
-      fprintf(stderr, "merylFileReader::nextMer()-- Expected 0x7461446c7972656d got 0x%016" F_X64P "\n", m1);
-      fprintf(stderr, "merylFileReader::nextMer()-- Expected 0x0a3030656c694661 got 0x%016" F_X64P "\n", m2);
+    if ((m1 != M1) ||
+        (m2 != M2)) {
+      fprintf(stderr, "dumpMerylDataFile()-- Magic number mismatch at position " F_U64 ".\n", position);
+      fprintf(stderr, "dumpMerylDataFile()-- Expected 0x%016" F_X64P " got\n", M1);
+      fprintf(stderr, "dumpMerylDataFile()            0x%016" F_X64P "\n", m1);
+      fprintf(stderr, "dumpMerylDataFile()-- Expected 0x%016" F_X64P " got\n", M2);
+      fprintf(stderr, "dumpMerylDataFile()            0x%016" F_X64P "\n", m2);
       exit(1);
     }
 
@@ -118,6 +120,11 @@ dumpMerylDataFile(char *name) {
     uint64 c1         = D->getBinary(64);
     uint64 c2         = D->getBinary(64);
 
+    uint8  lCode      = D->getBinary(8);    //  Only in merylDataFile01!
+    uint32 labelBits  = D->getBinary(6);
+    uint64 l1         = D->getBinary(58);
+    uint64 l2         = D->getBinary(64);
+
     fprintf(stdout, "\n");
     fprintf(stdout, " kmerIdx prefixDelta      prefix |--- suffix-size and both suffixes ---|    value\n");
     fprintf(stdout, "-------- ----------- ----------- -- ---------------- -- ---------------- --------\n");
@@ -126,6 +133,7 @@ dumpMerylDataFile(char *name) {
     uint64   *s1 = new uint64 [nKmers];
     uint64   *s2 = new uint64 [nKmers];
     uint64   *va = new uint64 [nKmers];
+    uint64   *la = new uint64 [nKmers];
 
     uint32    ls = (binaryBits <= 64) ? (0)          : (binaryBits - 64);
     uint32    rs = (binaryBits <= 64) ? (binaryBits) : (64);
@@ -133,31 +141,45 @@ dumpMerylDataFile(char *name) {
     uint64    tp = 0;
 
     //  Get all the kmers.
-    for (uint32 kk=0; kk<nKmers; kk++) {
-      if (kCode == 1) {
+    if (kCode == 1)
+      for (uint32 kk=0; kk<nKmers; kk++) {
         pd[kk] = D->getUnary();
         s1[kk] = D->getBinary(ls);
         s2[kk] = D->getBinary(rs);
       }
 
-      else {
-        fprintf(stderr, "ERROR: unknown kCode %u\n", kCode), exit(1);
-      }
+    else {
+      fprintf(stderr, "ERROR: unknown kCode %u\n", kCode), exit(1);
     }
 
     //  Get all the values.
-    for (uint32 kk=0; kk<nKmers; kk++) {
-      if      (cCode == 1) {
+    if      (cCode == 1) {
+      for (uint32 kk=0; kk<nKmers; kk++)
         va[kk] = D->getBinary(32);
-      }
+    }
 
-      else if (cCode == 2) {
+    else if (cCode == 2) {
+      for (uint32 kk=0; kk<nKmers; kk++)
         va[kk] = D->getBinary(64);
-      }
+    }
 
-      else {
-        fprintf(stderr, "ERROR: unknown cCode %u\n", cCode), exit(1);
-      }
+    else {
+      fprintf(stderr, "ERROR: unknown cCode %u\n", cCode), exit(1);
+    }
+
+    //  Get all the labels.
+    if      (lCode == 0) {
+      for (uint32 kk=0; kk<nKmers; kk++)
+        la[kk] = 0;
+    }
+
+    else if (lCode == 1) {
+      for (uint32 kk=0; kk<nKmers; kk++)
+        la[kk] = D->getBinary(labelBits);
+    }
+
+    else {
+      fprintf(stderr, "ERROR: unknown lCode 0x%02x\n", lCode), exit(1);
     }
 
     //  Dump.
@@ -167,6 +189,12 @@ dumpMerylDataFile(char *name) {
       fprintf(stdout, "%8u %11lu %011lx %2u %016lx %2u %016lx %8lx\n",
               kk, pd[kk], tp, ls, s1[kk], rs, s2[kk], va[kk]);
     }
+
+    delete [] la;
+    delete [] va;
+    delete [] s2;
+    delete [] s1;
+    delete [] pd;
   }
 
   delete D;

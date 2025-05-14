d49454455023b3174ebed1860bb8e8a939aad535
Brian P. Walenz <thebri@gmail.com>
2025-05-14 12:47:36 -0400

    Tidy up increaseArray() and add more tests.

--- src/tests/arraysTest.C
+++ src/tests/arraysTest.C
@@ -27,91 +27,198 @@ int32
 main(int32 argc, char **argv) {
   uint32   Alen=0, Amax=0;   uint32  *A=nullptr;
   uint32   Blen=0, Bmax=0;   uint32  *B=nullptr;
-  uint32   Clen=0, Cmax=0;   uint32  *C=nullptr;
-  uint32   Dlen=0, Dmax=0;   uint32  *D=nullptr;
+  uint32   Clen=0, Cmax=0;   uint64  *C=nullptr;
+  uint32   Dlen=0, Dmax=0;   uint64  *D=nullptr;
 
   fprintf(stderr, "Testing increaseArray() and related.\n");
   fprintf(stderr, "For best results, run in valgrind.\n");
   fprintf(stderr, "\n");
-  fprintf(stderr, "Calling allocateArray().\n");
-
-  allocateArray(A, Amax, 4000);     //  Allocate new arrays, both will
-  allocateArray(B, Bmax = 4000);    //  set elements to zero by default.
-
-  for (uint32 ii=0; ii<4000; ii++)  //  Let valgrind check for overflow.
-    A[ii] = B[ii] = ii;
-
-  fprintf(stderr, "Testing allocateArray().\n");
-
-  for (uint32 ii=0; ii<4000; ii++) {
-    assert(A[ii] == ii);
-    assert(B[ii] == ii);
-  }
-
-  for (uint32 ii=0; ii<4000; ii++)  //  Let valgrind check for overflow.
-    A[ii] = ii << 8;
-
-  for (uint32 ii=0; ii<4000; ii++)
-    B[ii] = ii << 16;
-
-  Alen = 4000;   //  Set the length of the two arrays;
-  Blen = 4000;   //  necessary for duplicateArray() below!
 
   //
 
-  fprintf(stderr, "Calling duplicateArray().\n");
+  fprintf(stderr, "allocateArray()\n");
 
-  duplicateArray(C, Clen, Cmax, A, Alen, Amax);        //  Duplicate the arrays,
-  duplicateArray(D, Dlen, Dmax, B, Blen, Bmax);        //  the last call will force
-  duplicateArray(D, Dlen, Dmax, B, Blen, Bmax, true);  //  a reallocation.
+  allocateArray(A, Amax, 4000);       //  Allocate new arrays, both will
+  allocateArray(B, Bmax = 4000);      //  set elements to zero by default.
 
-  fprintf(stderr, "Testing duplicateArray().\n");
-  for (uint32 ii=0; ii<4000; ii++) {
-    assert(C[ii] == A[ii]);
-    assert(D[ii] == B[ii]);
-  }
+  fprintf(stderr, "  setting\n");
+
+  for (uint32 ii=0; ii<Amax; ii++)     A[ii] = ii << 8;
+  for (uint32 ii=0; ii<Bmax; ii++)     B[ii] = ii << 16;
+
+  fprintf(stderr, "  checking\n");
+
+  for (uint32 ii=0; ii<Amax; ii++)     assert(A[ii] == ii << 8);
+  for (uint32 ii=0; ii<Bmax; ii++)     assert(B[ii] == ii << 16);
+
+  fprintf(stderr, "  releasing\n");
+
+  delete [] A;  Amax = Alen = 0;  A = nullptr;
+  delete [] B;  Bmax = Blen = 0;  B = nullptr;
+  delete [] C;  Cmax = Clen = 0;  C = nullptr;
+  delete [] D;  Dmax = Dlen = 0;  D = nullptr;
+
+  //
+
+  fprintf(stderr, "increaseArray() from nullptr\n");
+
+  Alen = 3332;  increaseArray(A,    Alen, Amax, 3333, _raAct::copyDataClearNew);  assert(Amax == 3333);
+  Alen = 3333;  increaseArray(A,    Alen, Amax, 3333, _raAct::copyDataClearNew);  assert(Amax == 6666);
+  Blen = 3334;  increaseArray(B,    Blen, Bmax, 3333, _raAct::copyDataClearNew);  assert(Bmax == 6666);
+
+  Clen = 3332;  increaseArray(C, D, Clen, Cmax, 3333, _raAct::copyDataClearNew);  assert(Cmax == 3333);
+  Clen = 6667;  increaseArray(C, D, Clen, Cmax, 3333, _raAct::copyDataClearNew);  assert(Cmax == 9999);
+
+  fprintf(stderr, "  setting\n");
+
+  for (uint32 ii=0; ii<Alen; ii++)      A[ii] = ii << 8;
+  for (uint32 ii=0; ii<Blen; ii++)      B[ii] = ii << 16;
+  for (uint64 ii=0; ii<Clen; ii++)    { C[ii] = ii << 24;  D[ii] = ii << 32; }
+
+  fprintf(stderr, "  checking\n");
+
+  for (uint32 ii=0; ii<Alen; ii++)      assert(A[ii] == ii << 8);
+  for (uint32 ii=0; ii<Blen; ii++)      assert(B[ii] == ii << 16);
+  for (uint64 ii=0; ii<Clen; ii++)    { assert(C[ii] == ii << 24);
+                                        assert(D[ii] == ii << 32); }
+
+  for (uint32 ii=Alen; ii<Amax; ii++)   assert(A[ii] == 0);
+  for (uint32 ii=Blen; ii<Bmax; ii++)   assert(B[ii] == 0);
+  for (uint64 ii=Clen; ii<Cmax; ii++) { assert(C[ii] == 0);
+                                        assert(D[ii] == 0); }
+
+  fprintf(stderr, "  releasing\n");
+
+  //lete [] A;  Amax = Alen = 0;  A = nullptr;
+  delete [] B;  Bmax = Blen = 0;  B = nullptr;
+  //lete [] C;  Cmax = Clen = 0;  C = nullptr;
+  delete [] D;  Dmax = Dlen = 0;  D = nullptr;
+
+  //
+
+  fprintf(stderr, "duplicateArray() (lengths %u %u %u %u)\n", Alen, Blen, Clen, Dlen);
+
+  duplicateArray(B, Blen, Bmax, A, Alen, Amax);        //  Duplicate the arrays,
+  duplicateArray(D, Dlen, Dmax, C, Clen, Cmax);        //  the last call will force
+  duplicateArray(D, Dlen, Dmax, C, Clen, Cmax, true);  //  a reallocation.
+
+  assert(Blen == Alen);
+  assert(Bmax == Alen);
+
+  assert(Dlen == Clen);
+  assert(Dmax == Clen);
+
+  fprintf(stderr, "  checking\n");
+
+  for (uint32 ii=0; ii<Blen; ii++)  assert(B[ii] == A[ii]);
+  for (uint64 ii=0; ii<Dlen; ii++)  assert(D[ii] == C[ii]);
 
   //
 
   fprintf(stderr, "Testing increaseArray(single).\n");
 
-  for (uint32 ii=0; ii<6000; ii++) {
-    increaseArray(A, ii, Amax, 511, _raAct::copyDataClearNew);
+  Alen *= 2;
+  for (uint32 ii=0; ii<Alen; ii++) {
+    increaseArray(A, ii, Amax, 3333, _raAct::copyDataClearNew);
     A[ii] = ii;
   }
 
-  fprintf(stderr, "  Reallocated A from 4000 to 6000 with max %lu\n", Amax);
+  fprintf(stderr, "  Reallocated A from Alen=%u to Alen=%u, now with Amax %lu\n", Alen/2, Alen, Amax);
 
-  for (uint32 ii=0; ii<6000; ii++)
+  for (uint32 ii=0; ii<Alen; ii++)
     assert(A[ii] == ii);
-  for (uint32 ii=6000; ii<Amax; ii++)
+  for (uint32 ii=Alen; ii<Amax; ii++)
     assert(A[ii] == 0);
+
+  fprintf(stderr, "  releasing\n");
+
+  delete [] A;  Amax = Alen = 0;  A = nullptr;
+  delete [] B;  Bmax = Blen = 0;  B = nullptr;
+  delete [] C;  Cmax = Clen = 0;  C = nullptr;
+  delete [] D;  Dmax = Dlen = 0;  D = nullptr;
 
   //
 
   fprintf(stderr, "Testing increaseArray(pair).\n");
 
-  for (uint32 ii=0; ii<6000; ii++) {
-    increaseArrayPair(C, D, ii, Cmax, 511, _raAct::copyDataClearNew);
+  Clen *= 2;
+  for (uint64 ii=0; ii<Clen; ii++) {
+    increaseArrayPair(C, D, ii, Cmax, 3333, _raAct::copyDataClearNew);
     C[ii] = ii << 8;
     D[ii] = ii << 16;
+
+    //fprintf(stderr, "ii = %u %u %u -- %lu %lu\n", ii, ii << 8, ii << 16, C[ii], D[ii]);
+
+    //if (ii > 0) {
+    //  fprintf(stderr, "     %u %u %u -- %lu %lu\n", ii-1, (ii-1) << 8, (ii-1) << 16, C[ii-1], D[ii-1]);
+    //  assert(C[ii-1] == (ii-1) << 8);
+    //  assert(D[ii-1] == (ii-1) << 16);
+    //}
   }
 
-  fprintf(stderr, "  Reallocated C and D from 4000 to 6000 with max %lu\n", Cmax);
+  fprintf(stderr, "  Reallocated C and D from Clen=%u to Clen=%u, now with Cmax %lu\n", Clen/2, Clen, Cmax);
 
-  for (uint32 ii=0; ii<6000; ii++) {
+  for (uint64 ii=0; ii<Clen; ii++) {
     assert(C[ii] == ii << 8);
     assert(D[ii] == ii << 16);
   }
-  for (uint32 ii=6000; ii<Cmax; ii++) {
+
+  fprintf(stderr, "  testing zero\n");
+
+  for (uint64 ii=Clen; ii<Cmax; ii++) {
     assert(C[ii] == 0);
     assert(D[ii] == 0);
   }
 
-  delete [] A;
-  delete [] B;
-  delete [] C;
-  delete [] D;
+  delete [] A;  Amax = Alen = 0;  A = nullptr;
+  delete [] B;  Bmax = Blen = 0;  B = nullptr;
+  delete [] C;  Cmax = Clen = 0;  C = nullptr;
+  delete [] D;  Dmax = Dlen = 0;  D = nullptr;
+
+  //
+
+  fprintf(stderr, "Testing increaseArray(pair) zero.\n");
+
+  increaseArrayPair(C, D, 0, Cmax, 3333, _raAct::copyDataClearNew);
+  increaseArrayPair(C, D, 1, Cmax, 3333, _raAct::copyDataClearNew);
+  increaseArrayPair(C, D, 0, Cmax, 3333, _raAct::copyDataClearNew);
+
+  fprintf(stderr, "Testing increaseArray(pair) stepped.\n");
+
+  for (Clen=10000; Clen < 110000; Clen += 10000) {
+    fprintf(stderr, "  step from %lu to %lu\n", Clen-10000, Clen);
+    increaseArrayPair(C, D, Clen, Cmax, 3333, _raAct::copyDataClearNew);
+    fprintf(stderr, "  step from %lu to %lu cmax now %u\n", Clen-10000, Clen, Cmax);
+  }
+  Clen -= 10000;  //  Last valid allocation!
+
+  assert(Clen <= Cmax);
+
+  fprintf(stderr, "  setting (Clen = %u)\n", Clen);
+
+  for (uint64 ii=0; ii<Clen; ii++) {
+    C[ii] = ii << 8;
+    D[ii] = ii << 16;
+  }
+
+  fprintf(stderr, "  testing\n");
+
+  for (uint64 ii=0; ii<Clen; ii++) {
+    assert(C[ii] == ii << 8);
+    assert(D[ii] == ii << 16);
+  }
+
+  fprintf(stderr, "  testing zero\n");
+
+  for (uint64 ii=Clen; ii<Cmax; ii++) {
+    assert(C[ii] == 0);
+    assert(D[ii] == 0);
+  }
+
+  delete [] A;  Amax = Alen = 0;  A = nullptr;
+  delete [] B;  Bmax = Blen = 0;  B = nullptr;
+  delete [] C;  Cmax = Clen = 0;  C = nullptr;
+  delete [] D;  Dmax = Dlen = 0;  D = nullptr;
 
   return(0);
 }
